67e70269a3e41aea58ff624b2395384968dd4b40
Carleigh Crockett <capleugh@gmail.com>
2019-09-17 10:27:51 -0600

    Adds method and test for find_photographs_by_artist

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -8,31 +8,50 @@ class CuratorTest < Minitest::Test
   def setup
     @curator = Curator.new
     @photo_1 = Photograph.new({
-        id: "1",
-        name: "Rue Mouffetard, Paris (Boy with Bottles)",
-        artist_id: "4",
-        year: "1954"
-      })
-      @photo_2 = Photograph.new({
-        id: "2",
-        name: "Moonrise, Hernandez",
-        artist_id: "2",
-        year: "1941"
-      })
-      @artist_1 = Artist.new({
-        id: "1",
-        name: "Henri Cartier-Bresson",
-        born: "1908",
-        died: "2004",
-        country: "France"
-      })
-      @artist_2 = Artist.new({
-        id: "2",
-        name: "Ansel Adams",
-        born: "1902",
-        died: "1984",
-        country: "United States"
-      })
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "4",
+      year: "1954"
+    })
+    @photo_2 = Photograph.new({
+      id: "2",
+      name: "Moonrise, Hernandez",
+      artist_id: "2",
+      year: "1941"
+    })
+    @photo_3 = Photograph.new({
+      id: "3",
+      name: "Identical Twins, Roselle, New Jersey",
+      artist_id: "3",
+      year: "1967"
+    })
+    @photo_4 = Photograph.new({
+      id: "4",
+      name: "Monolith, The Face of Half Dome",
+      artist_id: "3",
+      year: "1927"
+    })
+    @artist_1 = Artist.new({
+      id: "1",
+      name: "Henri Cartier-Bresson",
+      born: "1908",
+      died: "2004",
+      country: "France"
+    })
+    @artist_2 = Artist.new({
+      id: "2",
+      name: "Ansel Adams",
+      born: "1902",
+      died: "1984",
+      country: "United States"
+    })
+    @artist_3 = Artist.new({
+      id: "3",
+      name: "Diane Arbus",
+      born: "1923",
+      died: "1971",
+      country: "United States"
+    })
   end
 
   def test_it_exists
@@ -68,5 +87,20 @@ class CuratorTest < Minitest::Test
   def test_photograph_can_be_found_by_id
     @curator.add_photograph(@photo_2)
     assert_equal @photo_2, @curator.find_photograph_by_id("2")
+
+    @curator.add_photograph(@photo_1)
+    assert_equal @photo_1, @curator.find_photograph_by_id("1")
+  end
+
+  def test_photograph_can_be_found_by_artist
+    @curator.add_artist(@artist_1)
+    @curator.add_artist(@artist_2)
+    @curator.add_artist(@artist_3)
+    @curator.add_photograph(@photo_1)
+    @curator.add_photograph(@photo_2)
+    @curator.add_photograph(@photo_3)
+    @curator.add_photograph(@photo_4)
+
+    assert_equal [@photo_3, @photo_4], @curator.find_photographs_by_artist(@artist_3)
   end
 end
